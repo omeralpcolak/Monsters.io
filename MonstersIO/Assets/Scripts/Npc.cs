f29d0bd2588148151461b76d7efd1102e2bbb3fe
Ömer Alp Çolak <omer.alp.colak@gmail.com>
2024-03-18 01:11:34 +0300

    Little update on MainScene

--- MonstersIO/Assets/Scripts/Npc.cs
+++ MonstersIO/Assets/Scripts/Npc.cs
@@ -1,16 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class Npc : MonoBehaviour
 {
     public GameObject holdingMenu;
+    private CanvasGroup menuCanvasGroup;
+
+
+    private void Start()
+    {
+        menuCanvasGroup = holdingMenu.GetComponent<CanvasGroup>();
+    }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
             holdingMenu.SetActive(true);
+            menuCanvasGroup.DOFade(1, .5f);
         }
     }
 
@@ -18,7 +27,12 @@ public class Npc : MonoBehaviour
     {
         if (other.gameObject.CompareTag("Player"))
         {
-            holdingMenu.SetActive(false);
+            menuCanvasGroup.DOFade(0f, .5f).OnComplete(delegate
+            {
+                holdingMenu.SetActive(false);
+            });
+
+            
         }
     }
 
