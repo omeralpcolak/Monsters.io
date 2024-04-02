5dcb5c5077d1c5db0d3d422be3c7e85c89cd5d93
Ömer Alp Çolak <omer.alp.colak@gmail.com>
2024-04-03 00:27:32 +0300

    Update GameSceneManager.cs

--- MonstersIO/Assets/Scripts/GameSceneManager.cs
+++ MonstersIO/Assets/Scripts/GameSceneManager.cs
@@ -1,13 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class GameSceneManager : MonoBehaviour
 {
     public static GameSceneManager instance;
+    public GameState State;
+    public static Action<GameState> OnGameStateChange;
 
     private void Start()
     {
         instance = this;
     }
+
+
+    public void ChangeGameState(GameState newState)
+    {
+        State = newState;
+        switch (newState)
+        {
+            case GameState.PlAYING:
+                break;
+            case GameState.WIN:
+                break;
+            case GameState.LOSE:
+                break;
+            case GameState.PAUSE:
+                break;
+        }
+
+        OnGameStateChange?.Invoke(newState);
+    }
+
+}
+
+public enum GameState
+{
+    PAUSE,
+    LOSE,
+    WIN,
+    PlAYING
 }
