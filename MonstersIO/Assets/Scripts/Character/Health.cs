0e2301e64c780d751b7731e178b6a69da7ca44b9
Ömer Alp Çolak <omer.alp.colak@gmail.com>
2024-04-11 15:05:09 +0300

    Update Health.cs

--- MonstersIO/Assets/Scripts/Character/Health.cs
+++ MonstersIO/Assets/Scripts/Character/Health.cs
@@ -2,15 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System;
+using UnityEngine.UI;
 
 public class Health : MonoBehaviour
 {
     public Action<bool, int> listener;
     public int hp;
+    private Slider healthbar;
+
+    private void Start()
+    {
+        healthbar = GetComponentInChildren<Slider>();
+        UpdateHealthbar(hp);
+    }
 
     public void TakeDamage(int _damage)
     {
         hp -= _damage;
+        UpdateHealthbar(hp);
         listener?.Invoke(hp <= 0 ? true : false, _damage);
+    }
+
+    private void UpdateHealthbar(int amount)
+    {
+        healthbar.maxValue = amount;
     }
 }
