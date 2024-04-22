5e5578fb4ce32c9aa5bf8070ffbd658ba0a12de6
Ömer Alp Çolak <omer.alp.colak@gmail.com>
2024-04-22 18:26:57 +0300

    Update DamageUI.cs

--- MonstersIO/Assets/Scripts/DamageUI.cs
+++ MonstersIO/Assets/Scripts/DamageUI.cs
@@ -9,18 +9,20 @@ public class DamageUI : MonoBehaviour
 {
     public TMP_Text damageTxt;
     public float dissolveTime;
-
+    public List<Color> colors;
     private void Start()
     {
+        int randomIndex = Random.Range(0, colors.Count);
         float amount = Random.Range(-.5f, .5f);
-
         Vector2 moveVector = new Vector2(transform.position.x + amount, transform.position.y +1);
-
         transform.DOMove(moveVector, dissolveTime)
 
             .OnStart(delegate
             {
-                GetComponent<CanvasGroup>().DOFade(0, dissolveTime).OnComplete(() => Destroy(gameObject));
+                damageTxt.DOColor(colors[randomIndex], dissolveTime).OnComplete(() =>
+                {
+                    GetComponent<CanvasGroup>().DOFade(0, dissolveTime/2).OnComplete(() => Destroy(gameObject));
+                });
             });
         
     }
